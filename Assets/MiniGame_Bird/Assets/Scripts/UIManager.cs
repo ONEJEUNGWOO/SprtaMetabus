8e8deeadc0951361575077af3adf14903186fcb9
원정우 <rjstn155@naver.com>
2025-07-26 23:00:05 +0900

    Feature: 미니게임 점수 저장 기능 구현

--- Assets/MiniGame_Bird/Assets/Scripts/UIManager.cs
+++ Assets/MiniGame_Bird/Assets/Scripts/UIManager.cs
@@ -7,8 +7,18 @@ using UnityEngine.UI;
 public class UIManagerBird : MonoBehaviour
 {
     public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI nowScoreText;
+    public TextMeshProUGUI bestScoreText;
+    public GameObject scoreBord;
     public Button restartButton;
     public Button returnButton;
+
+    int bestScore = 0;
+    public int BestScore { 
+        get { return bestScore; } 
+        set { if (value <= bestScore) return; else bestScore = value; } }
+
+    private const string BestScoreKey = "BestScore";
 
     // Start is called before the first frame update
     void Start()
@@ -17,21 +27,33 @@ public class UIManagerBird : MonoBehaviour
         if (scoreText == null)
             Debug.LogError("score null");
 
+        bestScore = PlayerPrefs.GetInt(BestScoreKey, 0);
+        bestScoreText.text = bestScore.ToString();
+
         restartButton.gameObject.SetActive(false);
         returnButton.gameObject.SetActive(false);
-
-
+        scoreBord.SetActive(false);
     }
 
     public void SetRestart()
     {
         restartButton.gameObject.SetActive(true);
         returnButton.gameObject.SetActive(true);
+        scoreBord.SetActive(true);
     }
 
     public void UpdateScore(int score)
     {
+        if (score > bestScore)
+        {
+            bestScore = score;
+            PlayerPrefs.SetInt(BestScoreKey, score);
+            PlayerPrefs.Save();
+        }
+
         scoreText.text = score.ToString();
+        bestScoreText.text = bestScore.ToString();
+        nowScoreText.text = score.ToString();
     }
 
 }
